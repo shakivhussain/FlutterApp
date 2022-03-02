c53518be2cd0c42c1bcc2f62801fb958e0d17d2c
Shakiv_Hussain <shakibraza436@gmail.com>
2022-03-02 14:13:09 +0530

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
- 
 // Most important method, every time call when we refresh the data
   @override
   Widget build(BuildContext context) {
+    // bringVegitables(); // method calling
 
-   
     return MaterialApp(
-      home: HomePage()
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home", // set custom page
+      routes: {
+        "/": (context) => LoginPage(), // default route ( intent )
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
+  }
+
+  bringVegitables({bool bag = false, int amount = 100}) {
+    //
   }
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Material(
+      child: Center(
+        child: Text(
+          "Login Screen",
+          style: TextStyle(
+              fontSize: 40, color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
